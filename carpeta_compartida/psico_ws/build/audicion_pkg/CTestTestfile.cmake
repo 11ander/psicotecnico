1eebddafef8d8e3e6b432cda9a8d3b21602013d9
11ander <ander.p@opendeusto.es>
2025-10-02 17:20:40 +0200

    workspace done

---
+++ carpeta_compartida/psico_ws/build/audicion_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robotica_tiago/carpeta_compartida/psico_ws/src/audicion_pkg
+# Build directory: /home/robotica_tiago/carpeta_compartida/psico_ws/build/audicion_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ carpeta_compartida/psico_ws/build/vision_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robotica_tiago/carpeta_compartida/psico_ws/src/vision_pkg
+# Build directory: /home/robotica_tiago/carpeta_compartida/psico_ws/build/vision_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
