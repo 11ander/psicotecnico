6a9cac78618ef6ebe55d4541e42db6fa3bb8d38a
11ander <ander.p@opendeusto.es>
2025-10-27 20:54:06 +0100

    mapa hecho y navegacion funcionando desde rviz

--- carpeta_compartida/setup_env.sh
+++ carpeta_compartida/setup_env.sh
@@ -1,3 +1,7 @@
 source ~/carpeta_compartida/gallium/setup.bash
 export ROS_MASTER_URI=http://tiago-222c:11311
+<<<<<<< HEAD
 export ROS_IP=10.68.0.136
+=======
+export ROS_IP=10.68.0.133
+>>>>>>> af26a8d (mapa hecho y navegacion funcionando desde rviz)
